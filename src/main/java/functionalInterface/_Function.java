a86e7aeea69d9e1d8de3bf155ff732d200654d25
elias <provinciaelias@gmail.com>
2024-01-22 11:18:31 -0500

    featÃÃ: function use a method andThen

--- src/main/java/functionalInterface/_Function.java
+++ src/main/java/functionalInterface/_Function.java
@@ -8,11 +8,22 @@ public class _Function {
         System.out.println(increment);
 
         /**
-         * here use a function with a method apply this receive an int
+         * here use a function with a method apply this receive an int , this funciona incremento by 1
          */
         int increment3 = incrementByOneFunction.apply(1);
         System.out.println(increment3);
+        /**
+         * here we use a function that multiply by 10
+         */
+        int multiply = multiplyBy10Function.apply(increment3);
+        System.out.println(multiply);
 
+        /**
+         * here create a function that increment a number by 1 and then multiply by 10, using a past functions
+         */
+        Function<Integer,Integer> addBy1AndThenMultiplyBy10 =incrementByOneFunction.andThen(multiplyBy10Function);
+
+        System.out.println( addBy1AndThenMultiplyBy10.apply(4));
     }
 
     /**
@@ -20,6 +31,10 @@ public class _Function {
      */
     static Function<Integer,Integer> incrementByOneFunction= number->number+1;
 
+    /**
+     * this fuction multiply a number by 10 and return that result
+     */
+    static Function<Integer,Integer> multiplyBy10Function= number->number*10;
     /**
      * this a simple method that takes an int and returns an int
      * @param num
